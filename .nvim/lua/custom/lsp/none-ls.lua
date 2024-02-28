fa5900ba586d21e2e58a3eba21ae026b93da3180
Jiyeol Lee <ka.felix496@gmail.com>
2024-02-27 23:01:30 -0500

    remove deprecated config

--- .nvim/lua/custom/lsp/none-ls.lua
+++ .nvim/lua/custom/lsp/none-ls.lua
@@ -6,8 +6,6 @@ M.config = function()
   local null_ls = require("null-ls")
   -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
   local formatting = null_ls.builtins.formatting
-  -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-  local diagnostics = null_ls.builtins.diagnostics
   local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
   local setup = {
     debug = false,
@@ -18,7 +16,6 @@ M.config = function()
       formatting.gofumpt,
       formatting.goimports,
       formatting.prismaFmt,
-      diagnostics.shellcheck,
     },
     on_attach = function(client, bufnr)
       if client.supports_method("textDocument/formatting") then
