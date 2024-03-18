23d319906aa19e94e8967759df940db18b878b87
Jiyeol Lee <ka.felix496@gmail.com>
2024-03-18 13:55:16 -0400

    use default vim-airline

--- .nvim/lua/custom/airline.lua
+++ .nvim/lua/custom/airline.lua
@@ -1,5 +1,5 @@
 local M = {
-  "kafelix496/vim-airline",
+  "vim-airline/vim-airline",
   -- not going to use vim dev icons for this. it's not actually helps me
   -- dependencies = {
   --  "ryanoasis/vim-devicons"
@@ -9,8 +9,7 @@ local M = {
 M.config = function()
   vim.cmd [[
     let g:airline#extensions#tabline#enabled = 1
-    " let g:airline#extensions#tabline#formatter = 'unique_tail'
-    let g:airline#extensions#tabline#formatter = 'unique_tail_customized'
+    let g:airline#extensions#tabline#formatter = 'unique_tail'
     let g:airline#extensions#tabline#tab_nr_type = 1
     let g:airline#extensions#tabline#show_buffers = 0
     let g:airline#extensions#tabline#show_close_button = 0
