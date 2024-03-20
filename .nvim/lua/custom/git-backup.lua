d9bb67c9fc6136a80880f91947492d7c1f79e6a9
Jiyeol Lee <ka.felix496@gmail.com>
2024-03-20 14:51:58 -0400

    update the message handle of git backup

--- .nvim/lua/custom/git-backup.lua
+++ .nvim/lua/custom/git-backup.lua
@@ -21,24 +21,106 @@ function Git_backup()
   local notify_title = 'Git Processing'
 
   if output == commit_message then
-    notify('Amending last commit with new changes', vim.log.levels.INFO, { title = notify_title })
+    notify(
+      'Amending last commit with new changes',
+      vim.log.levels.INFO,
+      {
+        title = notify_title,
+        render = 'wrapped-compact'
+      }
+    )
     os.execute('git add -A && git commit --allow-empty --amend --no-edit')
-    notify('Pushing changes to remote', vim.log.levels.INFO, { title = notify_title })
+    notify(
+      'Pushing changes to remote',
+      vim.log.levels.INFO,
+      {
+        title = notify_title,
+        render = 'wrapped-compact'
+      }
+    )
+
+    local error_message = nil
+
     vim.fn.jobstart({ 'git', 'push', '--force-with-lease' }, {
-      stdout = false,
-      on_exit = function()
-        notify('Pushed changes to remote', vim.log.levels.INFO, { title = notify_title })
-      end
+      stdout_buffered = true,
+      stderr_buffered = true,
+      on_stderr = function(_, data)
+        error_message = data
+      end,
+      on_exit = function(_, exit_code)
+        if exit_code == 0 then
+          notify(
+            'Changes pushed to remote',
+            vim.log.levels.SUCCESS,
+            {
+              title = notify_title,
+              render = 'wrapped-compact'
+            }
+          )
+        else
+          if error_message then
+            notify(
+              error_message,
+              vim.log.levels.ERROR,
+              {
+                title = notify_title,
+                render = 'wrapped-compact'
+              }
+            )
+          end
+        end
+      end,
     })
   else
-    notify('Creating new commit', vim.log.levels.INFO, { title = notify_title })
+    notify(
+      'Creating new commit',
+      vim.log.levels.INFO,
+      {
+        title = notify_title,
+        render = 'wrapped-compact'
+      }
+    )
     os.execute('git add -A && git commit -m "' .. commit_message .. '"')
-    notify('Pushing changes to remote', vim.log.levels.INFO, { title = notify_title })
+    notify(
+      'Pushing changes to remote',
+      vim.log.levels.INFO,
+      {
+        title = notify_title,
+        render = 'wrapped-compact'
+      }
+    )
+
+    local error_message = nil
+
     vim.fn.jobstart({ 'git', 'push', '--force-with-lease' }, {
-      stdout = false,
-      on_exit = function()
-        notify('Pushed changes to remote', vim.log.levels.INFO, { title = notify_title })
-      end
+      stdout_buffered = true,
+      stderr_buffered = true,
+      on_stderr = function(_, data)
+        error_message = data
+      end,
+      on_exit = function(_, exit_code)
+        if exit_code == 0 then
+          notify(
+            'Changes pushed to remote',
+            vim.log.levels.SUCCESS,
+            {
+              title = notify_title,
+              render = 'wrapped-compact'
+            }
+          )
+        else
+          if error_message then
+            notify(
+              error_message,
+              vim.log.levels.ERROR,
+              {
+                title = notify_title,
+                render = 'wrapped-compact'
+              }
+            )
+          end
+        end
+      end,
     })
   end
 end
