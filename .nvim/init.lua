66d5785f14ffd7174744f52aee81a2cfb4e156b2
Jisung Lee <jskurt9352@gmail.com>
2024-06-21 22:25:59 +0900

    install nvim-scrollbar

--- .nvim/init.lua
+++ .nvim/init.lua
@@ -1,47 +1,47 @@
 -- this should be called first so we can use spec func
-require "custom.launch"
+require("custom.launch")
 
-require "custom.keymaps"
-require "custom.options"
-require "custom.autocommands"
-require "custom.git-backup"     -- to backup git repo
-require "custom.git-force-sync" -- to force sync git repo
-
+require("custom.keymaps")
+require("custom.options")
+require("custom.autocommands")
+require("custom.git-backup") -- to backup git repo
+require("custom.git-force-sync") -- to force sync git repo
 
 -- lsp
-spec "custom.lsp.mason"
-spec "custom.lsp.none-ls"
-spec "custom.lsp.lsp-signature"
+spec("custom.lsp.mason")
+spec("custom.lsp.none-ls")
+spec("custom.lsp.lsp-signature")
 
-spec "custom.codesnap"
-spec "custom.whichkey"
-spec "custom.notify"
-spec "custom.typescript"
-spec "custom.colorscheme"
-spec "custom.comment"
-spec "custom.vimwiki"
-spec "custom.highlightyark"
+spec("custom.codesnap")
+spec("custom.whichkey")
+spec("custom.notify")
+spec("custom.typescript")
+spec("custom.colorscheme")
+spec("custom.comment")
+spec("custom.vimwiki")
+spec("custom.highlightyark")
 -- spec "custom.octo"
-spec "custom.treesitter"
-spec "custom.nvim-tree"
-spec "custom.telescope"
-spec "custom.airline"
-spec "custom.gitsigns"
-spec "custom.floaterm"
-spec "custom.copilot"
-spec "custom.cmp"
-spec "custom.autopairs" -- this should be after cmp
-spec "custom.test"
-spec "custom.markdown-preview"
-spec "custom.bookmarks"
-spec "custom.prisma" -- tree sitter is not working for some reason
+spec("custom.treesitter")
+spec("custom.nvim-tree")
+spec("custom.telescope")
+spec("custom.airline")
+spec("custom.gitsigns")
+spec("custom.floaterm")
+spec("custom.copilot")
+spec("custom.cmp")
+spec("custom.autopairs") -- this should be after cmp
+spec("custom.test")
+spec("custom.markdown-preview")
+spec("custom.bookmarks")
+spec("custom.prisma") -- tree sitter is not working for some reason
+spec("custom.nvim-scrollbar")
 
 -- vim extra combinations start
-spec "custom.subversive"
-spec "custom.surround"
-spec "custom.replace-with-register"
+spec("custom.subversive")
+spec("custom.surround")
+spec("custom.replace-with-register")
 
 -- search and replace text
-spec "custom.spectre"
+spec("custom.spectre")
 
-require "custom.lazy" -- this should be the last
+require("custom.lazy") -- this should be the last
